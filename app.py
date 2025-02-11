62877fa987e28844065eed71be664c23ee60eb1d
Pasindu <pasindusangeeth968@gmail.com>
2025-02-11 13:04:01 +0530

    Fixed app.py by removing all Jupyter Notebook code

--- app.py
+++ app.py
@@ -1,52 +1,11 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-import yfinance as yf
 from datetime import datetime
 from src.data_collector import StockDataCollector
 from src.data_preprocessor import DataPreprocessor
 from src.model import StockPricePredictor
 from src.evaluator import ModelEvaluator
-
-# Set the time period for data
-start_date = datetime(2015, 1, 1)
-end_date = datetime.now()
-
-# Download stock data 
-stock_symbol = 'AAPL' # Apple stock
-df = yf.download(stock_symbol, start=start_date, end=end_date)
-
-# Save the data
-df.to_csv('stock_data.csv')
-
-# Display first few rows of the data
-print(df.head())
-
-plt.style.use('fivethirtyeight')
-%matplotlib inline
-
-# StockDataCollector object 
-collector = StockDataCollector()
-
-# get stock data
-symbol = 'AAPL'  # apple stock
-start = datetime(2020, 1, 1)  # from 2020
-end = datetime.now()  # till now
-
-# get stock data
-data = collector.get_stock_data(symbol, start, end)
-
-# print first 5 rows of data
-print("\nFirst 5 rows of data:")
-print(data.head())
-
-plt.style.use('fivethirtyeight')
-%matplotlib inline
-
-start = datetime(2015, 1, 1)
-end = datetime(2025, 1, 1)
-
-df = data.DataReader('AAPL', 'yahoo', start, end)
 
 def main():
     try:
